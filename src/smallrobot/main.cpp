1a52757fa98fe17d0ca2778fb4e4dacb45f5e63f
2025aperakal <149544026+2025aperakal@users.noreply.github.com>
2024-07-11 16:55:52 -0400

    Update main.cpp

--- src/smallrobot/main.cpp
+++ src/smallrobot/main.cpp
@@ -11,7 +11,7 @@ int main()
 	//    MOTOR             MOTOR                CONST                CONST            CONST                       CONST                        ANALOG            ANALOG             SERVO             SERVO            CONST              CONST               SERVO
 	//int leftWheelPin, int rightWheelPin, float wheelDistance, float wheelRadius, int leftTicksPerRevolution, int rightTicksPerRevolution, int leftColorPin, int rightColorPin, int clawServoPin, int armServoPin, int leftThreshold, int rightThreshold, int flickerServoPin
     SmallRobot robot(0,                 1,                 6.5,             1.375,                       1900,                        1900,                0,                 1,                1,               0,              3800,               3800,                   2);
-    //robot.rotateAndCorrect(90, 1000);
+    //robot.set_servo_position(this->clawServoPin, 300);
     disable_servos();
 
     return 0;
