80162fcb7b9a973a5464d8242ef630070a4881fc
2025aperakal <149544026+2025aperakal@users.noreply.github.com>
2024-07-11 16:05:19 -0400

    Add files via upload

---
+++ include/kipr/smallRobot.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <kipr/wombat.h>
+
+class SmallRobot
+{
+private:
+    float wheelRadius;   // Both are in inches
+    float wheelDistance; // Distance between the wheels across the robot body; the length of the axle connecting the two wheels if there was an axle
+    int leftWheelPin;
+    int rightWheelPin;
+    int leftTicksPerRevolution;
+    int rightTicksPerRevolution;
+    int leftPosPerOneInch;
+    int rightPosPerOneInch;
+    int leftLightPin;
+    int rightLightPin;
+    int clawServoPin;
+    int armServoPin;
+    int leftThreshold;
+    int rightThreshold;
+    int flickerServoPin;
+
+public:
+    SmallRobot(int leftWheelPin, int rightWheelPin, float wheelDistance, float wheelRadius, int leftTicksPerRevolution, int rightTicksPerRevolution, int leftColorPin, int rightColorPin, int clawServoPin, int armServoPin, int leftThreshold, int rightThreshold);
+
+    void moveDistanceAndCorrect(float distance, int ticksPerSecond, bool condition = true); // Moves for some distance, blocking
+    void moveDistance(float distance, int ticksPerSecond, bool condition = true);           // Same as moveForwardDistanceAndCorrect, but does not correct
+
+    void moveContinuous(int ticksPerSecond);    // Moves continously forward/backwards. percentPower -100 to 100
+    void moveUntilEitherColorDetect(int speed); // Blocking, and goes forward until either light sensors see black lines
+
+    void rotateAndCorrect(int degrees, int ticksPerSecond, bool condition = true); // Rotates a certain amount of degrees clockwise and corrects. Accepts values from -360 to 360. Blocking.
+    void rotate(int degrees, int percentPower, bool condition = true);             // Rotates a certain amount of degrees clockwise. Accepts values from -360 to 360. Blocking.
+
+    void turnLeftContinuous(int ticksPerSecond);  // Turns left continously. Percent power 0-100
+    void turnRightContinuous(int ticksPerSecond); // Turns right continously. Percent power 0-100
+
+    void openClaw(); //Opens claw for astronaut collection
+    void closeClaw();//Closes claw
+    void restClaw();//Claw 
+    void flickRight(int timeToFlick);//Moves the front flicker to the right
+    void flickLeft(int timeToFlick);//
+    void setArmPosition(int pos, int speed); // 0-2047, 0 is fully up, 2047 is down. Speed is pos per milisecond, and is positive.
+
+    void stop(); // Stops all turns and forward/backward movement.
+
+    bool leftColor();
+    bool rightColor();
+
+    void turnLeftWithColorSensor(int speed);
+    void turnRightWithColorSensor(int speed);
+
+    // TODO: ADD SMALL ROBOT SPECIFIC FUNCTIONS LATER
+};
